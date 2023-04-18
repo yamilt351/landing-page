fd3df6fc9e2d69f87f0c009c3ad3358305933922
yamil351 <leokasper@live.com.ar>
2023-04-18 14:50:45 -0300

    adding-function

--- src/Components/Product/product.jsx
+++ src/Components/Product/product.jsx
@@ -4,15 +4,37 @@ import './product.css';
 function Product() {
   const [wines, setWines] = useState(false);
   const [pizza, setPizza] = useState(true);
+  const [reviews, setReviews] = useState(false);
   return (
     <section className="product-section" id="Product">
       <div className="title-container">
-        <h1>Pizzas</h1>
-        <h1>Wines</h1>
-        <h1>Reviews</h1>
+        <h1
+          className={pizza ? 'selected' : 'hide'}
+          onClick={() => {
+            !setPizza;
+          }}
+        >
+          Pizzas
+        </h1>
+        <h1
+          className={wines ? 'selected' : 'hide'}
+          onClick={() => {
+            !setWines;
+          }}
+        >
+          Wines
+        </h1>
+        <h1
+          className={reviews ? 'selected' : 'hide'}
+          onClick={() => {
+            !setReviews;
+          }}
+        >
+          Reviews
+        </h1>
       </div>
       <div className="product-container">
-        <Galery pizza={pizza} wine={wines}/>
+        <Galery pizza={pizza} wine={wines} />
       </div>
     </section>
   );
