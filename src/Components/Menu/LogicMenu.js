04e71e165794619fa5e26532b047c87d0ccce2eb
yamil351 <leokasper@live.com.ar>
2023-04-19 16:50:25 -0300

    modal-strict

---
+++ src/Components/Menu/LogicMenu.js
@@ -0,0 +1,14 @@
+import piza from '../images/pizza.webp';
+import piza2 from '../images/pizza.jpg';
+import wine from '../images/vino-argentino3.jpg';
+
+function SetNewImages(setCurrentImage) {
+  const images = [piza, piza2, wine];
+  let counter = 1;
+  const intervalId = setInterval(() => {
+    setCurrentImage(images[counter % images.length]);
+    counter++;
+  }, 5000);
+  return () => clearInterval(intervalId);
+}
+export default SetNewImages;
--- src/Components/Menu/menu.jsx
+++ src/Components/Menu/menu.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import SetNewImages from './LogicMenu';
 import piza from '../images/pizza.webp';
-import piza2 from '../images/pizza.jpg';
-import wine from '../images/red-wine-is-poured-into-a-glass-from-a-bottle--light-background--1153158143-98320451802c485cb6d7b5437c7fd60a.jpg';
+
 import './menu.css';
 
 function Menu() {
   const [currentImage, setCurrentImage] = useState(piza);
+
   useEffect(() => {
-    const images = [piza, piza2, wine];
-    let counter = 1;
-    const intervalId = setInterval(() => {
-      setCurrentImage(images[counter % images.length]);
-      counter++;
-    }, 5000);
-    return () => clearInterval(intervalId);
+    SetNewImages(setCurrentImage);
   }, []);
   return (
     <section className="section" id="Home">
