324cb4789435be57707dd49182bbdd0f7e3a1ceb
yamil351 <leokasper@live.com.ar>
2023-04-19 16:06:37 -0300

    animation

--- src/Components/Menu/menu.jsx
+++ src/Components/Menu/menu.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import piza from '../images/pizza.webp';
+import piza2 from '../images/pizza.jpg';
+import wine from '../images/red-wine-is-poured-into-a-glass-from-a-bottle--light-background--1153158143-98320451802c485cb6d7b5437c7fd60a.jpg';
 import './menu.css';
+
 function Menu() {
+  const [currentImage, setCurrentImage] = useState(piza);
+  useEffect(() => {
+    const images = [piza, piza2, wine];
+    let counter = 1;
+    const intervalId = setInterval(() => {
+      setCurrentImage(images[counter % images.length]);
+      counter++;
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
-    <section className="section" id='Home'>
+    <section className="section" id="Home">
       <div className="section-container">
         <div className="text-container">
           <h1>Lorem, ipsum.</h1>
@@ -14,7 +27,7 @@ function Menu() {
           <button> Lorem, ipsum dolor.</button>
         </div>
         <div className="image-container">
-          <img src={piza} alt="delicious_pizza" className="piza" />
+          <img src={currentImage} alt="delicious_pizza" className="piza" />
         </div>
       </div>
     </section>
--- src/Components/Modal/modal.jsx
+++ src/Components/Modal/modal.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './modal.css';
 
 function Modal({ isOpen, onClose }) {
+  const [signin, setSignin] = useState(true);
+
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay">
       <div className="modal">
-        <button onClick={onClose} className='close-button'>X</button>
-        <h2>Sign In</h2>
+        <button onClick={onClose} className="close-button">
+          X
+        </button>
+        <h2>{signin ? 'Sign In !' : 'Sign Up !'}</h2>
         <form>
           <label>
-            <input type="text" name="username" placeholder="Username" />
+            <input type="text" name="username" placeholder="E-mail" />
           </label>
           <label>
             <input type="password" name="password" placeholder="Password" />
           </label>
-          <button type="submit">Sign In</button>
+          <button type="submit" className="auth-button">
+            Submit
+          </button>
+          <button
+            type="submit"
+            className="auth-button"
+            onClick={(e) => {
+              e.preventDefault(), setSignin(!signin);
+            }}
+          >
+            {signin ? 'Create Account' : 'I already have an Account'}
+          </button>
         </form>
       </div>
     </div>
