47317527a60b2b9fe906ce6c2eed7854c3e7e166
yamil351 <leokasper@live.com.ar>
2023-04-23 18:19:54 -0300

    adding-regex

--- src/Components/Modal/loginModal.jsx
+++ src/Components/Modal/loginModal.jsx
@@ -4,7 +4,33 @@ import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 function LoginModal({ onClose }) {
   const [signin, setSignin] = useState(true);
   const [showPassword, setShowPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
 
+  const validateFields = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const passwordRegex = /^[^\s]{6,}$/
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address');
+      return;
+    } else {
+      setEmailError('');
+    }
+
+    if (!passwordRegex.test(password)) {
+      setPasswordError('minimum 6 characters');
+      return;
+    } else {
+      setPasswordError('');
+    }
+
+    // If all fields are valid, proceed with login request
+    console.log('All fields are valid. Sending login request...');
+  };
   return (
     <div>
       <div className="modal">
@@ -12,9 +38,10 @@ function LoginModal({ onClose }) {
           X
         </button>
         <h2>{signin ? 'Sign In !' : 'Sign Up !'}</h2>
-        <form>
+        <form onSubmit={validateFields}>
           <label>
             <input type="text" name="email" placeholder="E-mail" />
+            <div className="error-message">{emailError}</div>
           </label>
           <label>
             <input
@@ -22,15 +49,19 @@ function LoginModal({ onClose }) {
               name="username"
               placeholder="Username"
               className={signin ? 'hide' : ''}
+              title="example@email.com"
             />
           </label>
-          <label className='password-container'>
+          <label className="password-container">
             <input
               type={showPassword ? 'text' : 'password'}
               name="password"
               placeholder="Password"
+              title="The password must start with & and have at least 7 characters that can be letters, numbers, or some special characters."
             />
-            <div className='password-icon'
+
+            <div
+              className="password-icon"
               onClick={() => {
                 setShowPassword(!showPassword);
               }}
@@ -38,6 +69,7 @@ function LoginModal({ onClose }) {
               {showPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
             </div>
           </label>
+          <div className="error-message">{passwordError}</div>
           <button type="submit" className="auth-button">
             Submit
           </button>
