05d1d67618eeec8787117b8f35d477aaf3c28638
yamil351 <leokasper@live.com.ar>
2023-04-22 22:12:03 -0300

    'succes-form'

--- src/Components/Modal/cartModal.jsx
+++ src/Components/Modal/cartModal.jsx
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { CartContext } from '../Context/cartContext.jsx';
-
+import { GiConfirmed } from 'react-icons/gi';
 import './cart.css';
 import { BsFillCartDashFill } from 'react-icons/bs';
 
 function ShoppingCart({ onCartClose }) {
   const [confirmed, setConfirmed] = useState(false);
   const [cart, setCart] = useContext(CartContext);
+  const [order, setOrder] = useState(false);
 
   const quantity = cart.reduce((acc, curr) => {
     return acc + curr.quantity;
@@ -40,12 +41,13 @@ function ShoppingCart({ onCartClose }) {
     });
 
     if (isValid) {
-      alert('we recieved your order!');
+      setOrder(true);
       console.log('OK');
     } else {
       alert('invalid data');
     }
   };
+
   const handleCheckOrder = () => {
     console.log(cart.length);
     if (cart.length > 0) {
@@ -60,60 +62,78 @@ function ShoppingCart({ onCartClose }) {
       <button onClick={onCartClose} className="close-button">
         X
       </button>
-      <div className="cart-title-container">
-        <h2 className="cart-title">{confirmed ? 'Address' : 'Your Order'}</h2>
+      <div className={order ? 'order-recieved-modal' : 'hide'}>
+        <div className="cart-title-container">
+          <h2 className="cart-title">Your order has been received</h2>
+        </div>
+        <div className="success-icon-container">
+          <GiConfirmed />
+        </div>
       </div>
-      <div className="cart-items-container">
-        <div className="cart-items">
-          <ul className={confirmed ? 'cart-link hide' : 'cart-links'}>
-            <div>Items: {quantity}</div>
-            {cart.map((item) => {
-              return (
-                <li key={item.id} className="item-list">
-                  <span className="item-name">{item.dish}</span>
-                  <span className="item-separator">:.......</span>
-                  <span className="item-quantity">{item.quantity}</span>
-                  <span className="item-delete">
-                    <BsFillCartDashFill onClick={() => removeItem(item.id)}>
-                      Remove
-                    </BsFillCartDashFill>
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
-          <div className={confirmed ? 'user-data-form' : 'hide'}>
-            <label htmlFor="">Name *</label>
-            <input type="text" placeholder="Name" name="name" required />
-            <label htmlFor="">Phone / Mobile *</label>
-            <input
-              type="tel"
-              placeholder="Phone / Mobile"
-              name="phone"
-              required
-            />
-            <label htmlFor="">Address *</label>
-            <input type="text" placeholder="Address" name="address" required />
-            <label htmlFor="">Annotations *</label>
-            <textarea
-              type="text"
-              placeholder="Annotations"
-              cols={43}
-              rows={10}
-              name="apartment"
-              required
-            />
-          </div>
+      <div className={order ? 'hide' : 'cart-container'}>
+        <div className="cart-title-container">
+          <h2 className="cart-title">{confirmed ? 'Address' : 'Your Order'}</h2>
+        </div>
+        <div className="cart-items-container">
+          <div className="cart-items">
+            <ul className={confirmed ? 'cart-link hide' : 'cart-links'}>
+              <div>Items: {quantity}</div>
+              {cart.map((item) => {
+                return (
+                  <li key={item.id} className="item-list">
+                    <span className="item-name">{item.dish}</span>
+                    <span className="item-separator">:.......</span>
+                    <span className="item-quantity">{item.quantity}</span>
+                    <span className="item-delete">
+                      <BsFillCartDashFill onClick={() => removeItem(item.id)}>
+                        Remove
+                      </BsFillCartDashFill>
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className={confirmed ? 'user-data-form' : 'hide'}>
+              <label htmlFor="">Name *</label>
+              <input type="text" placeholder="Name" name="name" required />
+              <label htmlFor="">Phone / Mobile *</label>
+              <input
+                type="tel"
+                placeholder="Phone / Mobile"
+                name="phone"
+                required
+              />
+              <label htmlFor="">Address *</label>
+              <input
+                type="text"
+                placeholder="Address"
+                name="address"
+                required
+              />
+              <label htmlFor="">Annotations *</label>
+              <textarea
+                type="text"
+                placeholder="Annotations"
+                cols={43}
+                rows={10}
+                name="apartment"
+                required
+              />
+            </div>
 
-          <div className="total-price">Total: ${totalPrice}</div>
-          <div className="order-buttons">
-            {' '}
-            <button onClick={handleCheckOrder}>
-              {confirmed ? 'Edit Order' : 'Confirm Order'}
-            </button>
-            <button onClick={handleConfirm} className={confirmed ? '' : 'hide'}>
-              {confirmed ? 'Confirm' : ''}
-            </button>
+            <div className="total-price">Total: ${totalPrice}</div>
+            <div className="order-buttons">
+              {' '}
+              <button onClick={handleCheckOrder}>
+                {confirmed ? 'Edit Order' : 'Confirm Order'}
+              </button>
+              <button
+                onClick={handleConfirm}
+                className={confirmed ? '' : 'hide'}
+              >
+                {confirmed ? 'Confirm' : ''}
+              </button>
+            </div>
           </div>
         </div>
       </div>
